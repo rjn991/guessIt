ae14bb3046464eb232d7276e81fc6d7e1e60452a
rjn991 <ranjanht991@gmail.com>
2024-09-13 05:19:12 +0530

    added footer

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -1,10 +1,13 @@
-import classes from './Footer.module.css'
-const Footer =() => {
-    return (<div>
-        <div className={classes.footerWrapper}>
+import classes from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <div>
+      <img className={classes.svg} src="/src/assets/wave.svg"></img>
+      <div className={classes.footerWrapper}>
         <p>Copyleft 2024</p>
-        </div>
-        
-    </div>)
-}
-export default Footer
+      </div>
+    </div>
+  );
+};
+export default Footer;
