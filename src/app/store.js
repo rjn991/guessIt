98f05b1fcf1f233a02e73a59557b817a8b3d926d
rjn991 <ranjanht991@gmail.com>
2024-11-02 21:15:03 +0530

    added redux-persist to fix browser reload error

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,36 @@
-import { configureStore } from '@reduxjs/toolkit'
-import playlistReducer from '../features/playlistSlice'
+import { configureStore } from "@reduxjs/toolkit";
+import playlistReducer from "../features/playlistSlice";
+import storage from "redux-persist/lib/storage";
+import {
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import { combineReducers } from "@reduxjs/toolkit";
+import { version } from "react";
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const reducer = combineReducers({
+  playlist: playlistReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, reducer);
 
 export const store = configureStore({
-  reducer: {
-    playlist: playlistReducer,
-  },
-})
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { store } from './app/store'
-import { Provider } from 'react-redux'
+import { store } from "./app/store";
+import { Provider } from "react-redux";
 
 import App from "./App.jsx";
 import Question from "./Question/Question.jsx";
@@ -10,10 +10,10 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Result from "./Result/Result.jsx";
 import Finish from "./Finish/Finish.jsx";
 
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
 
-
-
-
+let persistor = persistStore(store);
 
 const router = createBrowserRouter([
   {
@@ -25,19 +25,21 @@ const router = createBrowserRouter([
     element: <Question></Question>,
   },
   {
-    path:"/result",
-    element:<Result></Result>
+    path: "/result",
+    element: <Result></Result>,
   },
   {
-    path:"/finish",
-    element:<Finish></Finish>
-  }
+    path: "/finish",
+    element: <Finish></Finish>,
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
   <Provider store={store}>
-    <RouterProvider router={router} />
+    <PersistGate persistor={persistor}>
+      <RouterProvider router={router} />
+    </PersistGate>
   </Provider>
   // </React.StrictMode>
 );
